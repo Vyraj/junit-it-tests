61722845006b1fa895cacb03656941676bf0ff59
Michal Bodzek <michal.bodzek@wp.pl>
2020-03-15 16:38:43 +0100

    Initial commit

---
+++ src/main/java/com/vyraj/junitandittests/JunitAndItTestsApplication.java
@@ -0,0 +1,13 @@
+package com.vyraj.junitandittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitAndItTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitAndItTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vyraj/junitandittests/JunitAndItTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vyraj.junitandittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitAndItTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
